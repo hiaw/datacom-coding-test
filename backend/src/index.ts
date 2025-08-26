e0b7241803ee9cf5a09006bc5ef32752a7b15c7c
Daniel Chong <danielgq.chong@gmail.com>
2025-08-27 08:50:11 +1200

    feat: backend code for creating and getting order.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from "hono"
+import { HTTPException } from "hono/http-exception"
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+interface Order {
+  status: "Pending" | "Processing" | "Completed"
+  id: number
+  name: string
+  price: number
+}
+
+const orders: Order[] = []
+
+app.get("/order", (c) => {
+  return c.json(orders)
+})
+
+app.post("/order", async (c) => {
+  const order: Order = await c.req.json()
+  if (order && order.id && order.name && order.price) {
+    order.status = "Pending"
+    orders.push(order)
+  } else {
+    throw new HTTPException(401, { message: "Invalid order" })
+  }
+
+  return c.json(order)
 })
 
 export default app
